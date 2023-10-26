45b854280bc9a2b8a9c564aac0e78e7e288dfa4d
forellko <dragonforce97q@gmail.com>
2023-10-26 21:01:33 +0300

    Create Add Product Page

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function AddProductPage() {
+  return (
+    <div>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input input-bordered mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="input input-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input input-bordered mb-3 w-full"
+        />
+        <button type="submit" className="btn btn-primary w-full">
+          Add Product
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddProductPage;
