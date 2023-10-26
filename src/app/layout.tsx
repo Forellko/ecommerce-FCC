68643f98f9e82bfc0acc93252316a246a5171282
forellko <dragonforce97q@gmail.com>
2023-10-26 21:06:15 +0300

    Margin auto to center main content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="p-4">{children}</main>
+        <main className="m-auto max-w-7xl p-4">{children}</main>
       </body>
     </html>
   );
