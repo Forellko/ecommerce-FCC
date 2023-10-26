e05384c54e19533cf65261e2bedc0dd8bec83944
forellko <dragonforce97q@gmail.com>
2023-10-27 01:46:02 +0300

    Create product card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import ProductCard from "@/components/ProductCard";
+import prisma from "@/lib/db/prisma";
 import Image from "next/image";
 
-export default function Home() {
-  return <div></div>;
+export default async function Home() {
+  const products = await prisma.product.findMany({ orderBy: { id: "desc" } });
+  return (
+    <div>
+      <ProductCard product={products[0]} />
+    </div>
+  );
 }
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,32 @@
+import { Product } from "@prisma/client";
+import Link from "next/link";
+import PriceTag from "./PriceTag";
+import Image from "next/image";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export default function ProductCard({ product }: ProductCardProps) {
+  return (
+    <Link
+      href={"/products/" + product.id}
+      className="card w-full bg-base-100 transition-shadow hover:shadow-xl"
+    >
+      <figure>
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={800}
+          height={400}
+          className="h-48 object-cover"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{product.name}</h2>
+        <p>{product.description}</p>
+        <PriceTag price={product.price} />
+      </div>
+    </Link>
+  );
+}
