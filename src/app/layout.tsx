2e28386b9b9b3ed043fc1efc88efc89b837805b9
forellko <dragonforce97q@gmail.com>
2023-10-26 21:03:10 +0300

    Add main padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="p-4">{children}</main>
+      </body>
     </html>
   );
 }
