20785d003ef32f4670632751499e3913b38c1fc9
forellko <dragonforce97q@gmail.com>
2023-10-27 01:52:23 +0300

    Add NEW badge

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -8,6 +8,10 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
+  const isNew =
+    Date.now() - new Date(product.createdAt).getTime() <
+    1000 * 60 * 60 * 24 * 7;
+
   return (
     <Link
       href={"/products/" + product.id}
@@ -23,7 +27,10 @@ export default function ProductCard({ product }: ProductCardProps) {
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{product.name}</h2>
+        <h2 className="card-title">
+          {product.name}{" "}
+          {isNew && <div className="badge badge-secondary">NEW</div>}
+        </h2>
         <p>{product.description}</p>
         <PriceTag price={product.price} />
       </div>
