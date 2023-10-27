314d29a2aa1f3f2838a3d3f5cc504ff42a6a0a84
forellko <dragonforce97q@gmail.com>
2023-10-28 00:57:20 +0300

    AddToCartButton incrementProductQuantity

--- src/app/products/[id]/AddToCartButton.tsx
+++ src/app/products/[id]/AddToCartButton.tsx
@@ -1,13 +1,31 @@
 "use client";
 
+import { useState, useTransition } from "react";
+
 interface AddToCartButtonProps {
   productId: string;
+  incrementProductQuantity: (productId: string) => Promise<void>;
 }
 
-export default function AddToCartButton({ productId }: AddToCartButtonProps) {
+export default function AddToCartButton({
+  productId,
+  incrementProductQuantity,
+}: AddToCartButtonProps) {
+  const [isPending, startTransition] = useTransition();
+  const [success, setSuccess] = useState(false);
+
   return (
     <div className="flex items-center gap-2">
-      <button className="btn btn-primary" onClick={() => {}}>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          setSuccess(false);
+          startTransition(async () => {
+            await incrementProductQuantity(productId);
+            setSuccess(true);
+          });
+        }}
+      >
         Add to cart
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -24,6 +42,10 @@ export default function AddToCartButton({ productId }: AddToCartButtonProps) {
           />
         </svg>
       </button>
+      {isPending && <span className="loading loading-ring loading-md"></span>}
+      {!isPending && success && (
+        <span className="text-success">Added to cart.</span>
+      )}
     </div>
   );
 }
