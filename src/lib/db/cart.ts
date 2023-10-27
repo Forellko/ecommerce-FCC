efcb1f648705060847c086d07d08a64c6efe0960
forellko <dragonforce97q@gmail.com>
2023-10-27 23:53:49 +0300

    Create getCart and createCart

---
+++ src/lib/db/cart.ts
@@ -0,0 +1,41 @@
+import { Cart, Prisma } from "@prisma/client";
+import prisma from "./prisma";
+import { cookies } from "next/dist/client/components/headers";
+
+export type CartWithProducts = Prisma.CartGetPayload<{
+  include: { items: { include: { product: true } } };
+}>;
+
+export type ShoppingCart = CartWithProducts & {
+  size: number;
+  subtotal: number;
+};
+
+export async function getCart(): Promise<ShoppingCart | null> {
+  const localCartId = cookies().get("localCardId")?.value;
+  const cart = localCartId
+    ? await prisma.cart.findUnique({
+        where: { id: localCartId },
+        include: { items: { include: { product: true } } },
+      })
+    : null;
+
+  if (!cart) return null;
+
+  return {
+    ...cart,
+    size: cart.items.reduce((acc, item) => acc + item.quantity, 0),
+    subtotal: cart.items.reduce(
+      (acc, item) => acc + item.quantity * item.product.price,
+      0,
+    ),
+  };
+}
+
+export async function createCart(): Promise<ShoppingCart> {
+  const newCart = await prisma.cart.create({ data: {} });
+
+  cookies().set("localCartId", newCart.id);
+
+  return { ...newCart, size: 0, subtotal: 0, items: [] };
+}
