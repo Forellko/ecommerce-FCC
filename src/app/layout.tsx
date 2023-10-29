485d060545696a605122ac599665d79f8a8ab341
forellko <dragonforce97q@gmail.com>
2023-10-29 21:53:38 +0300

    Add Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
+        <Footer />
       </body>
     </html>
   );
