c7035ac3454ef4356ac1198a3c1e57e1b8998fae
forellko <dragonforce97q@gmail.com>
2023-10-31 01:51:07 +0300

    Finish card page

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -4,15 +4,19 @@ import { CartItemWithProduct } from "@/lib/db/cart";
 import { formatPrice } from "@/lib/format";
 import Image from "next/image";
 import Link from "next/link";
-import { ReactElement } from "react";
+import { ReactElement, useTransition } from "react";
 
 interface CartEntryProps {
   cartItem: CartItemWithProduct;
+  setProductQuantity: (productId: string, quantity: number) => Promise<void>;
 }
 
 export default function CartEntry({
   cartItem: { product, quantity },
+  setProductQuantity,
 }: CartEntryProps) {
+  const [isPending, startTransition] = useTransition();
+
   const quantityOptions: ReactElement[] = [];
   for (let i = 1; i < 99; i++) {
     quantityOptions.push(
@@ -41,14 +45,22 @@ export default function CartEntry({
           <select
             className="select select-bordered w-full max-w-[80px]"
             defaultValue={quantity}
-            onChange={e => }
+            onChange={(e) => {
+              const newQuantity = parseInt(e.currentTarget.value);
+
+              startTransition(async () => {
+                await setProductQuantity(product.id, newQuantity);
+              });
+            }}
           >
+            <option value={0}>0</option>
             {quantityOptions}
           </select>
         </div>
         <div className="flex items-center gap-3">
           Total: {formatPrice(product.price * quantity)}
         </div>
+        {isPending && <span className="loading loading-dots loading-sm" />}
       </div>
       <div className="divider" />
     </div>
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,7 @@
 import { getCart } from "@/lib/db/cart";
 import CartEntry from "./CartEntry";
+import { setProductQuantity } from "./actions";
+import { formatPrice } from "@/lib/format";
 
 export const metadata = {
   title: "Your Cart - Flowmazon",
@@ -12,8 +14,21 @@ export default async function CartPage() {
     <div>
       <h1 className="mb-6 text-3xl font-bold">Shopping cart</h1>
       {cart?.items.map((cartItem) => {
-        return <CartEntry cartItem={cartItem} key={cartItem.id} />;
+        return (
+          <CartEntry
+            cartItem={cartItem}
+            key={cartItem.id}
+            setProductQuantity={setProductQuantity}
+          />
+        );
       })}
+      {cart?.items.length === 0 && <p>Cart is empty.</p>}
+      <div className="flex flex-col items-end sm:items-center">
+        <p className="mb-3 font-bold">
+          Total: {formatPrice(cart?.subtotal || 0)}
+        </p>
+        <button className="btn btn-primary sm:w-[200px]">Checkout</button>
+      </div>
     </div>
   );
 }
