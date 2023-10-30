33158e2fb0d0f36158fb81e3360e26f5470f7d73
forellko <dragonforce97q@gmail.com>
2023-10-31 01:12:18 +0300

    Add quantity select

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -4,6 +4,7 @@ import { CartItemWithProduct } from "@/lib/db/cart";
 import { formatPrice } from "@/lib/format";
 import Image from "next/image";
 import Link from "next/link";
+import { ReactElement } from "react";
 
 interface CartEntryProps {
   cartItem: CartItemWithProduct;
@@ -12,6 +13,15 @@ interface CartEntryProps {
 export default function CartEntry({
   cartItem: { product, quantity },
 }: CartEntryProps) {
+  const quantityOptions: ReactElement[] = [];
+  for (let i = 1; i < 99; i++) {
+    quantityOptions.push(
+      <option value={i} key={i}>
+        {i}
+      </option>,
+    );
+  }
+
   return (
     <div>
       <div className="flex flex-wrap items-center gap-3">
@@ -26,6 +36,16 @@ export default function CartEntry({
       <div>
         <Link href={"/products/" + product.id}>{product.name}</Link>
         <div>Price: {formatPrice(product.price)}</div>
+        <div className="my-1 flex items-center gap-2">
+          Quantity:
+          <select
+            className="select select-bordered w-full max-w-[80px]"
+            defaultValue={quantity}
+            onChange={e => }
+          >
+            {quantityOptions}
+          </select>
+        </div>
         <div className="flex items-center gap-3">
           Total: {formatPrice(product.price * quantity)}
         </div>
