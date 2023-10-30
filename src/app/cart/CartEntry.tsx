545d03c31bb494b714b150422e032b4f9d20b389
forellko <dragonforce97q@gmail.com>
2023-10-31 01:00:10 +0300

    Add price and link

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { CartItemWithProduct } from "@/lib/db/cart";
+import { formatPrice } from "@/lib/format";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CartEntryProps {
   cartItem: CartItemWithProduct;
@@ -20,6 +22,13 @@ export default function CartEntry({
           height={200}
           className="rounded-lg"
         />
+      </div>
+      <div>
+        <Link href={"/products/" + product.id}>{product.name}</Link>
+        <div>Price: {formatPrice(product.price)}</div>
+        <div className="flex items-center gap-3">
+          Total: {formatPrice(product.price * quantity)}
+        </div>
       </div>
       <div className="divider" />
     </div>
