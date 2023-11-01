08055a30ada3ad00b1443226c3d9705729f155c3
forellko <dragonforce97q@gmail.com>
2023-11-01 15:26:44 +0300

    Add redirect from add product page

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,6 +1,8 @@
 import FormSubmitButton from "@/components/FormSubmitButton";
 import prisma from "@/lib/db/prisma";
+import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export const metadata = {
   title: "Add Product - Flowmazon",
@@ -29,7 +31,13 @@ async function addProduct(formData: FormData) {
   redirect("/");
 }
 
-function AddProductPage() {
+async function AddProductPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("api/auth/signin?callbackUrl=/add-product");
+  }
+
   return (
     <div>
       <h1 className="mb-3 text-lg font-bold">Add Product</h1>
