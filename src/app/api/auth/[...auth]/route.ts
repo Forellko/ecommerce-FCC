f325ceaaba6111b6dfd70f704cc9831c7c8e16c1
forellko <dragonforce97q@gmail.com>
2023-11-01 09:55:29 +0300

    Add Google auth route

---
+++ src/app/api/auth/[...auth]/route.ts
@@ -0,0 +1,19 @@
+import prisma from "@/lib/db/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import Google from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
